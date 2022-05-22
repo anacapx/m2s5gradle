d543bd2636dd1840913ee5ccefc38210cc09f80c
amaral.carolina <w.anapereira@gmail.com>
2022-05-22 20:06:52 -0300

    First commit

---
+++ src/main/java/com/example/m2s5gradle/M2s5gradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.m2s5gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2s5gradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2s5gradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/m2s5gradle/M2s5gradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m2s5gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2s5gradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
